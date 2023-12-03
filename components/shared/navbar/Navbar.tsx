86b1130e0f5c804ca1f2da09dd4713afa2c3dc59
Pranav R <pranav81234@gmail.com>
2023-12-03 13:31:12 +0530

    navbar completed

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import { SignedIn, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Theme from './Theme';
+import MobileNav from './MobileNav';
+import GlobalSearch from '../search/GlobalSearch';
 
 const Navbar = () => {
   return (
@@ -18,9 +21,9 @@ const Navbar = () => {
           <span className="text-primary-500"> Flow</span>
         </p>
       </Link>
-      Global Seach
+      <GlobalSearch />
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
@@ -34,7 +37,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        {/* Mobile Nav */}
+        <MobileNav />
       </div>
     </nav>
   );
